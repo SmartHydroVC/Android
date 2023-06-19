5ad985fc1fee49884ad0f0ad2a1913746ace2d8d
Preshen4 <preshen4@gmail.com>
2023-06-19 20:55:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/smarthydro/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.smarthydro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.smarthydro.ui.theme.SmartHydroTheme
+import com.example.smarthydro.ui.theme.screen.home.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SmartHydroTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
+
