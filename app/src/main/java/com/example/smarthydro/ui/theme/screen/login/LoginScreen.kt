4fd61fd4347a53e88f6b6f00049d2ec3f1488102
Preshen4 <preshen4@gmail.com>
2023-07-04 04:37:12 +0200

    Login page

---
+++ app/src/main/java/com/example/smarthydro/ui/theme/screen/login/LoginScreen.kt
@@ -0,0 +1,235 @@
+package com.example.smarthydro.ui.theme.screen.login
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.smarthydro.R
+import com.example.smarthydro.R.*
+import com.example.smarthydro.ui.theme.*
+
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
+@ExperimentalComposeUiApi
+@Composable
+@Preview
+fun SigInScreen() {
+    var username by remember{ mutableStateOf("")}
+    var password by remember{ mutableStateOf("")}
+    val (focusUsername,focusPassword) = remember { FocusRequester.createRefs()}
+    val keyboardController =  LocalSoftwareKeyboardController.current
+    var isPasswordVisible by remember{mutableStateOf(false)}
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(fraction = 0.30f),
+                Alignment.TopEnd,
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_humidity), contentDescription = "",
+                    modifier = Modifier.fillMaxSize(), contentScale = ContentScale.FillBounds
+                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .padding(horizontal = 20.dp, vertical = 50.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_humidity),
+                        contentDescription = "Logo App",
+                        modifier = Modifier
+                            .weight(1f)
+                            .size(100.dp),
+                        colorFilter = ColorFilter.tint(Color.White)
+                    )
+                    Text(text = "Welcome", fontSize = 20.sp, color = Color.White)
+                }
+
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 40.dp)
+            ) {
+                Text(text = "Log in", style = MaterialTheme.typography.headlineLarge)
+                OutlinedTextField(value = username, onValueChange = { username = it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .focusRequester(focusUsername),
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                    keyboardActions = KeyboardActions(onNext = { focusPassword.requestFocus() }),
+                    singleLine = true,
+                    label = { Text(text = "Username") }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .focusRequester(focusPassword),
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text(text = "Password") },
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }),
+                    visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    trailingIcon = {
+                        IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                            Icon(
+                                imageVector = if (isPasswordVisible) drawable.ic_eye_open as ImageVector else drawable.ic_eye_close as ImageVector,
+                                contentDescription = "Password Toggle"
+                            )
+
+                        }
+                    }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(), Arrangement.SpaceBetween
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Checkbox(checked = true, onCheckedChange = {})
+                        Text(text = "Remember me", fontSize = 12.sp)
+                    }
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Forgot Password", fontSize = 12.sp)
+                    }
+
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp)
+                ) {
+                    Text(text = "Log in")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(), Arrangement.Center
+                ) {
+                    Text(text = "Or log in with", fontSize = 14.sp)
+                }
+                Row(modifier = Modifier.fillMaxWidth(), Arrangement.SpaceAround) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(50),
+                        modifier = Modifier
+                            .height(45.dp)
+                            .width(45.dp),
+                        colors = ButtonDefaults.buttonColors(Color.White)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_humidity),
+                            contentDescription = "Facebook logo",
+                            modifier = Modifier
+                                .weight(1f)
+                                .size(40.dp),
+                            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.secondary)
+                        )
+                    }
+                    Button(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(50),
+                        modifier = Modifier
+                            .height(45.dp)
+                            .width(45.dp),
+                        colors = ButtonDefaults.buttonColors(Color.White)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_humidity),
+                            contentDescription = "Google logo",
+                            modifier = Modifier
+                                .weight(1f)
+                                .size(40.dp),
+                            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.secondary)
+                        )
+                    }
+                    Button(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(50),
+                        modifier = Modifier
+                            .height(45.dp)
+                            .width(45.dp),
+                        colors = ButtonDefaults.buttonColors(Color.White)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_humidity),
+                            contentDescription = "Twitter logo",
+                            modifier = Modifier
+                                .weight(1f)
+                                .size(40.dp),
+                            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.secondary)
+                        )
+                    }
+
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "Don't have account?", fontSize = 14.sp)
+                    TextButton(onClick = { /*TODO*/ }) {
+                        Text(text = "Register")
+                    }
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+    }
+
+
