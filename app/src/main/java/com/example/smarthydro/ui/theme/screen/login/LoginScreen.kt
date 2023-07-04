b3077f44b1accdb1f9257c938e007922ef02bbd9
Preshen4 <preshen4@gmail.com>
2023-07-04 08:19:27 +0200

    Update LoginScreen.kt

--- app/src/main/java/com/example/smarthydro/ui/theme/screen/login/LoginScreen.kt
+++ app/src/main/java/com/example/smarthydro/ui/theme/screen/login/LoginScreen.kt
@@ -49,160 +49,108 @@ import com.example.smarthydro.R
 import com.example.smarthydro.R.*
 import com.example.smarthydro.ui.theme.*
 
-
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
-@ExperimentalComposeUiApi
 @Composable
-@Preview
-fun SigInScreen() {
-    Box(modifier = Modifier
-        .background(DeepBlue)
-        .fillMaxSize()
-    ) {
-    SignInContent()
-
+fun SignInScreen() {
+    var username by remember {
+        mutableStateOf("")
     }
-}
-@Composable
-fun SignInContent() {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            AppLogo()
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 40.dp)
-            ) {
-                LoginInFields()
-                Spacer(modifier = Modifier.height(16.dp))
-                CheckBoxes()
-                Spacer(modifier = Modifier.height(16.dp))
-                LoginButton()
-                Spacer(modifier = Modifier.height(16.dp))
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "Don't have account?", fontSize = 14.sp)
-                    TextButton(onClick = { /*TODO*/ }) {
-                        Text(text = "Register")
-                    }
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-            }
-        }
+    var password by remember {
+        mutableStateOf("")
+    }
+    var isPasswordVisible by remember {
+        mutableStateOf(false)
+    }
+    val isFormValid by derivedStateOf {
+        username.isNotBlank() && password.length >= 7
     }
 
-
-@Composable
-fun LoginButton() {
-    Button(
-        onClick = { /*TODO*/ },
-        modifier = Modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(16.dp)
-    ) {
-        Text(text = "Log in")
-    }
-
-}
-
-@Composable
-fun CheckBoxes() {
-    Row(
-        modifier = Modifier.fillMaxWidth(), Arrangement.SpaceBetween
-    ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Checkbox(checked = true, onCheckedChange = {})
-            Text(text = "Remember me", fontSize = 12.sp)
-        }
-        TextButton(onClick = { /*TODO*/ }) {
-            Text(text = "Forgot Password", fontSize = 12.sp)
-        }
-
-    }
-}
-
-@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
-@Composable
-fun LoginInFields() {
-    var username by remember{ mutableStateOf("")}
-    var password by remember{ mutableStateOf("")}
-    val (focusUsername,focusPassword) = remember { FocusRequester.createRefs()}
-    val keyboardController =  LocalSoftwareKeyboardController.current
-    var isPasswordVisible by remember{mutableStateOf(false)}
-    Text(text = "Log in", style = MaterialTheme.typography.headlineLarge)
-    OutlinedTextField(value = username, onValueChange = { username = it },
-        modifier = Modifier
-            .fillMaxWidth()
-            .focusRequester(focusUsername),
-        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
-        keyboardActions = KeyboardActions(onNext = { focusPassword.requestFocus() }),
-        singleLine = true,
-        label = { Text(text = "Username") }
-    )
-    Spacer(modifier = Modifier.height(8.dp))
-    OutlinedTextField(
-        modifier = Modifier
-            .fillMaxWidth()
-            .focusRequester(focusPassword),
-        value = password,
-        onValueChange = { password = it },
-        label = { Text(text = "Password") },
-        singleLine = true,
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Password,
-            imeAction = ImeAction.Done
-        ),
-        keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }),
-        visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-        trailingIcon = {
-            IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
-                Icon(
-                    imageVector = if (isPasswordVisible) drawable.ic_eye_open as ImageVector else drawable.ic_eye_close as ImageVector,
-                    contentDescription = "Password Toggle"
-                )
-
-            }
-        }
-    )
-}
-
-@Composable
-fun AppLogo() {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(fraction = 0.30f),
-        Alignment.TopEnd,
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_humidity), contentDescription = "",
-            modifier = Modifier.fillMaxSize(), contentScale = ContentScale.FillBounds
-        )
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .padding(horizontal = 20.dp, vertical = 50.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+    Scaffold(backgroundColor = MaterialTheme.colors.primary) {
+        Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Top) {
             Image(
-                painter = painterResource(id = R.drawable.ic_humidity),
-                contentDescription = "Logo App",
+                painter = painterResource(id = R.drawable.ic_visa),
+                contentDescription = "App Logo",
                 modifier = Modifier
                     .weight(1f)
-                    .size(100.dp),
+                    .size(200.dp),
                 colorFilter = ColorFilter.tint(Color.White)
             )
-            Text(text = "Welcome", fontSize = 20.sp, color = Color.White)
+            Card(
+                Modifier
+                    .weight(2f)
+                    .padding(8.dp),
+                shape = RoundedCornerShape(32.dp)
+            ) {
+                Column(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(32.dp)
+                ) {
+                    Text(text = "Welcome Back!", fontWeight = FontWeight.Bold, fontSize = 32.sp)
+                    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                        Spacer(modifier = Modifier.weight(1f))
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = username,
+                            onValueChange = { username = it },
+                            label = { Text(text = "Username") },
+                            singleLine = true,
+                            trailingIcon = {
+                                if (username.isNotBlank())
+                                    IconButton(onClick = { username = "" }) {
+                                        Icon(imageVector = Icons.Filled.Clear, contentDescription = "")
+                                    }
+                            }
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = password,
+                            onValueChange = { password = it },
+                            label = { Text(text = "Password") },
+                            singleLine = true,
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Done),
+                            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                            trailingIcon = {
+                                IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                                    Icon(
+                                        imageVector = if (isPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                                        contentDescription = "Password Toggle"
+                                    )
+                                }
+                            }
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(
+                            onClick = {},
+                            enabled = isFormValid,
+                            modifier = Modifier.fillMaxWidth(),
+                            shape = RoundedCornerShape(16.dp)
+                        ) {
+                            Text(text = "Log In")
+                        }
+                        Spacer(modifier = Modifier.weight(1f))
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            TextButton(onClick = {}) {
+                                Text(text = "Sign Up")
+                            }
+                            TextButton(onClick = { }) {
+                                Text(text = "Forgot Password?", color = Color.Gray)
+                            }
+                        }
+                    }
+                }
+            }
         }
-
     }
 }
 
-
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DefaultTheme {
+        SignInScreen()
+    }
+}
