2f50b4fd7319087accd592311376c068fc5b27d2
Yusufismail786 <101713732+Yusufismail786@users.noreply.github.com>
2023-11-10 12:50:01 +0200

    Added Bar Graph to ViewData

---
+++ app/src/main/java/com/example/smarthydro/ui/theme/screen/viewData/Chart.kt
@@ -0,0 +1,138 @@
+package com.example.smarthydro.ui.theme.screen.viewData
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+//import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.smarthydro.ui.theme.LightGreen1
+import com.example.smarthydro.ui.theme.Purple500
+
+//import com.example.customchar.ui.theme.Purple500
+
+@Composable
+fun Chart(
+    data: Map<Float, String>,
+    max_value: Int
+) {
+
+    val context = LocalContext.current
+    // BarGraph Dimensions
+    val barGraphHeight by remember { mutableStateOf(150.dp) }
+    val barGraphWidth by remember { mutableStateOf(20.dp) }
+    // Scale Dimensions
+    val scaleYAxisWidth by remember { mutableStateOf(20.dp) }
+    val scaleLineWidth by remember { mutableStateOf(2.dp) }
+
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 25.dp, vertical = 10.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(barGraphHeight),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            // scale Y-Axis
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(scaleYAxisWidth),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    Text(text = max_value.toString(), fontWeight = FontWeight.ExtraBold)
+                    Spacer(modifier = Modifier.fillMaxHeight())
+                }
+
+                Column(
+                    modifier = Modifier.fillMaxHeight(),
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    Text(text = (max_value / 2).toString(), fontWeight = FontWeight.ExtraBold)
+                    Spacer(modifier = Modifier.fillMaxHeight(0.5f))
+                }
+
+            }
+
+            // Y-Axis Line
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(scaleLineWidth)
+                    .background(LightGreen1)
+            )
+
+            // graph
+            data.forEach {
+                Box(
+                    modifier = Modifier
+                        .padding(start = barGraphWidth, bottom = 5.dp)
+                        .clip(CircleShape)
+                        .width(barGraphWidth)
+                        .fillMaxHeight(it.key)
+                        .background(Color.White)
+                        .clickable {
+                            Toast
+                                .makeText(context, it.key.toString(), Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                )
+            }
+
+        }
+
+        // X-Axis Line
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .fillMaxWidth()
+                .height(scaleLineWidth)
+                .background(LightGreen1)
+        )
+
+        // Scale X-Axis
+        Row(
+            modifier = Modifier
+                .padding(start = scaleYAxisWidth+barGraphWidth+scaleLineWidth)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(barGraphWidth)
+        ) {
+
+            data.values.forEach {
+                Text(
+                    modifier = Modifier.width(barGraphWidth),
+                    text = it,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            }
+
+        }
+
+    }
+
+}
