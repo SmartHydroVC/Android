d560796cee3554c6e5a55f262c6c8dabd9936c43
Preshen Govender <preshen4@gmail.com>
2023-11-16 09:33:14 +0200

    Update HomeScreen.kt

--- app/src/main/java/com/example/smarthydro/ui/theme/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/smarthydro/ui/theme/screen/home/HomeScreen.kt
@@ -176,6 +176,57 @@ fun  SensorCard(
             modifier = Modifier.padding(16.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
+            val width = 150
+            val height = 210
+
+            // Medium colored path
+            val mediumColoredPoint1 = Offset(0f, height * 0.3f)
+            val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
+            val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
+            val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
+            val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
+
+            val mediumColoredPath = Path().apply {
+                moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
+                standardQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
+                standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
+                standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
+                standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
+                lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+                lineTo(-100f, height.toFloat() + 100f)
+                close()
+            }
+
+            // Light colored path
+            val lightPoint1 = Offset(0f, height * 0.35f)
+            val lightPoint2 = Offset(width * 0.1f, height * 0.4f)
+            val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
+            val lightPoint4 = Offset(width * 0.65f, height.toFloat())
+            val lightPoint5 = Offset(width * 1.4f, -height.toFloat() / 3f)
+
+            val lightColoredPath = Path().apply {
+                moveTo(lightPoint1.x, lightPoint1.y)
+                standardQuadFromTo(lightPoint1, lightPoint2)
+                standardQuadFromTo(lightPoint2, lightPoint3)
+                standardQuadFromTo(lightPoint3, lightPoint4)
+                standardQuadFromTo(lightPoint4, lightPoint5)
+                lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+                lineTo(-100f, height.toFloat() + 100f)
+                close()
+            }
+            Canvas(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                drawPath(
+                    path = mediumColoredPath,
+                    color = feature.mediumColor
+                )
+                drawPath(
+                    path = lightColoredPath,
+                    color = feature.lightColor
+                )
+            }
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -184,7 +235,7 @@ fun  SensorCard(
             ) {
 
                 Text(
-                    text = "Gift Plant",
+                    text = feature.title,
                     fontSize =  24.sp,
                     style = MaterialTheme.typography.titleLarge,
                     fontWeight = FontWeight.SemiBold
@@ -210,7 +261,7 @@ fun  SensorCard(
                 modifier = Modifier.size(width = 100.dp, height = 140.dp)
             ) {
                 Text(
-                    text = "Image",
+                    text = "Reading",
                     modifier = Modifier.wrapContentSize(),
                     style = MaterialTheme.typography.titleMedium,
                 )
